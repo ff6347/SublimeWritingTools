a9fa5841722e5002102d6c86c94fde52dc33cba1
fabianmoronzirfas <fabian.moron.zirfas@gmail.com>
2017-06-13 11:46:47 +0200

    renamed figures file

---
+++ list_figures.py
@@ -0,0 +1,27 @@
+"""docstring"""
+import re
+import sublime
+import sublime_plugin
+
+from SublimeWritingTools.conf import settings
+
+RES = []
+# FIG = "foo"
+PREFIX = settings().get("prefix","+@")
+
+class ListFiguresCommand(sublime_plugin.TextCommand):
+    """docstrong"""
+
+    def on_done(self, index):
+        if index == -1:
+            return
+        self.view.run_command("insert", {"characters": PREFIX + RES[index]})
+
+    def run(self, edit):
+        contents = self.view.substr(sublime.Region(0, self.view.size()))
+        regex = r"(\#)(fig:.*?)(}| )"
+        matches = re.findall(regex, contents)
+        for match in list(matches):
+            RES.append(match[1])
+        win = sublime.active_window()
+        win.show_quick_panel(RES, self.on_done)
