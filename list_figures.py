6ab9173124c80c305297b7c3deb56559bb16597e
fabianmoronzirfas <fabian.moron.zirfas@gmail.com>
2017-06-14 09:57:32 +0200

    make the values unique in list figures

--- list_figures.py
+++ list_figures.py
@@ -7,7 +7,7 @@ from SublimeWritingTools.conf import settings
 
 RES = []
 # FIG = "foo"
-PREFIX = settings().get("prefix","+@")
+PREFIX = settings().get("prefix", "+@")
 
 class ListFiguresCommand(sublime_plugin.TextCommand):
     """docstrong"""
@@ -18,10 +18,12 @@ class ListFiguresCommand(sublime_plugin.TextCommand):
         self.view.run_command("insert", {"characters": PREFIX + RES[index]})
 
     def run(self, edit):
+        global RES
         contents = self.view.substr(sublime.Region(0, self.view.size()))
         regex = r"(\#)(fig:.*?)(}| )"
         matches = re.findall(regex, contents)
         for match in list(matches):
             RES.append(match[1])
+        RES = list(set(RES))
         win = sublime.active_window()
         win.show_quick_panel(RES, self.on_done)
